3b4b8434bc47e1c62d0201729855a9e1e8235baf
dias25080826 <dias25080826@gmail.com>
2026-09-02 16:32:12 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Depois de publicar o backend no Render, cole aqui a URL pública dele.
-// Exemplo: "https://fortaleza2028-api.onrender.com"
+// Exemplo: "https://nordeste-2028.onrender.com"
 const API_BASE_URL = "https://SUBSTITUA-PELA-SUA-URL-DO-RENDER.onrender.com";
